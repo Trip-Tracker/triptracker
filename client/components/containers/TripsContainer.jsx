83b3941ac10392973b76968d7e1db442df2ad88a
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-09-28 19:27:55 -0500

    Major front-end logic implmentations
    Co-authored-by: lagnum22 <lagnum22@users.noreply.github.com>
    Co-authored-by: dominicd17 <dominicd17@users.noreply.github.com>

--- client/components/containers/TripsContainer.jsx
+++ client/components/containers/TripsContainer.jsx
@@ -1,26 +1,68 @@
 import React from "react";
 import { Button, TextField, Grid, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
+import {connect} from 'react-redux';
+import { updateTrip } from "../../actions/actions";
+import * as types from "../../actions/actionTypes.js";
+
+const mapStateToProps = state => ({
+  tripsArray: state.mainReducer.tripsArray,
+  userEmail: state.mainReducer.userEmail
+});
+
+const mapDispatchToProps = dispatch => ({
+  selectTrip: (trip) => {
+    dispatch(updateTrip(trip));
+  }
+});
 
 const useStyles = makeStyles({
   btn: {
     backgroundColor: "#8e0000",
     color: "white",
   },
+  trips: {
+    backgroundColor: "#FFFFFF",
+    marginBottom: '10px'
+  }
 });
 
 
-function TripsContainer() {
+function TripsContainer(props) {
   const classes = useStyles();
+
+  let tripsToRender = [];
+  for (let i = 0; i < props.tripsArray.length; i++) {
+    const trip = props.tripsArray[i]
+    tripsToRender.push(
+      <Button
+        fullWidth 
+        variant="contained"
+        size="large"
+        className={classes.trips}
+        key = {`trip${i}`}
+        onClick={(e) => props.selectTrip(trip)}
+      >
+        {props.tripsArray[i].location} : {props.tripsArray[i].date}
+      </Button>
+    );
+  }
   return (
     <div className='graycard'>
       {/* <div className="mapContainer">
         <SimpleMap />
       </div> */}
-
-      <Button  variant="contained" className={classes.btn}>New Trip</Button>
+      <h1>Welcome {props.userEmail}</h1>
+      <h2> Trips </h2>
+    
+      <div className='tripsContainer'>
+        {tripsToRender}
+      </div>
+      <Button fullWidth variant="contained" className={classes.btn}>New Trip</Button>
     </div>
   );
 }
 
-export default TripsContainer;
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripsContainer);
